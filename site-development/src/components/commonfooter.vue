f6e8ca645e324a4ffc3d10a680de58ee417b5b31
luozhi1024 <13101606873@163.com>
2018-11-28 23:30:08 +0800

    site-development

---
+++ site-development/src/components/commonfooter.vue
@@ -0,0 +1,33 @@
+<template>
+<div></div>
+</template>
+<srcipt>
+  import "../../node_modules/bootstrap/dist/css/bootstrap.css";
+</srcipt>
+<style lang="scss" scoped>
+  .footer {
+    display: flex;
+    position: fixed;
+    top: 20%;
+    width: 100%;
+    height:100px;
+    line-height: 100px;
+    background-color: #222222;
+    font-size: 25px;
+  }
+  .footer-ul{
+    right: 0%;
+    display: flex;
+    position: fixed;
+  }
+  .title-menu{
+    color: #fff;
+    font-size: 30px;
+    margin-left: 50px;
+  }
+  .title-li{
+    margin-right: 60px;
+    color: #fff;
+  }
+
+</style>
