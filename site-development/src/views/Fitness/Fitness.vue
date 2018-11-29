8215fa170042967bfe5be8bb88bb1d725f48e124
luozhi1024 <13101606873@163.com>
2018-11-29 22:55:24 +0800

    site-development

---
+++ site-development/src/views/Fitness/Fitness.vue
@@ -0,0 +1,96 @@
+<template>
+  <div content="bg">
+    <img class="img-bg" src="../../assets/img/Fitness-bg.jpg">
+    <div class="text" align="center">
+      <div>我觉得吧</div>
+      <div>当然只是我觉得</div>
+      <div>每个人都是独立的个体吧</div>
+      <div>就是说每个人的生活总是会有不一样</div>
+      <div>我希望能分享你的乐趣</div>
+      <div>也希望我的乐趣呢</div>
+      <div>会带给你不一样的惊喜</div>
+    </div>
+    <div class="text-1" align="center">
+      <div>无忧无虑是三年</div>
+      <div>懵懵懂懂又三年</div>
+      <div>胡思乱想仍三年</div>
+      <div>命里无时终三年</div>
+    </div>
+    <div class="text-2" align="center">
+      <div>我呢</div>
+      <div>做不到一蓑烟雨任平生</div>
+      <div>心中又羡慕的紧</div>
+      <div>所以啊</div>
+      <div>人生如逆旅 我亦是行客</div>
+    </div>
+  </div>
+
+</template>
+
+
+<script>
+    import "vue-swipe/dist/vue-swipe.css";
+    import  {Swipe,SwipeItem} from "vue-swipe";
+    import MtSwipeItem from "vue-swipe/src/swipe-item";
+    export default {
+created(){
+            this.$emit("switchtab","book")
+        },
+        components:{
+            swipe : Swipe,
+            "swipe-item" : SwipeItem  ,
+
+
+        }
+    }
+    // 使用vue的自带组件案列
+</script>
+
+<style lang="scss" scoped>
+  .bg{
+    background-color: #0086b3;
+    height: 840px;
+
+  }
+
+  .img-bg{
+    background-size: 100% auto;
+    position: fixed;
+    z-index: -1;
+    width: 100%;
+  }
+  .text{
+    background-color:rgba(128,128,128,0.7);;
+    color: #87CEFA;
+    padding: 50px 40px 50px 40px;
+    width: 800px;
+    border-radius:40px;
+    font-size: 30px;
+    font-weight:400;
+  }
+  .text-1{
+    background-color:rgba(128,128,128 ,0.7);
+    color: #87CEFA;
+    right: 0;
+    float: right;
+    bottom: 0;
+    padding: 50px 40px 50px 40px;
+    width: 500px;
+    border-radius:40px;
+    font-size: 30px;
+    font-weight:400;
+  }
+  .text-2{
+    background-color:rgba(128,128,128 ,0.7);
+    color: #87CEFA;
+    bottom: 0;
+    padding: 50px 40px 50px 40px;
+    width: 500px;
+    border-radius:40px;
+    font-size: 30px;
+    font-weight:400;
+  }
+
+
+</style>
+
