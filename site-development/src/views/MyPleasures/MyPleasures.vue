4d0e37b245b7c9a423f6c4eadcdde443e919cc20
luozhi1024 <13101606873@163.com>
2018-12-04 23:27:47 +0800

    site-development

--- site-development/src/views/MyPleasures/MyPleasures.vue
+++ site-development/src/views/MyPleasures/MyPleasures.vue
@@ -1,27 +1,29 @@
 <template>
   <div content="bg">
     <img class="img-bg" src="../../assets/img/security-bg.jpg">
-    <div class="text" align="center">
-      <div>我觉得吧</div>
-      <div>当然只是我觉得</div>
-      <div>每个人都是独立的个体吧</div>
-      <div>就是说每个人的生活总是会有不一样</div>
-      <div>我希望能分享你的乐趣</div>
-      <div>也希望我的乐趣呢</div>
-      <div>会带给你不一样的惊喜</div>
+    <div class="text" >
+      <img style="width: 30%;float: right" src="../../assets/img/laugh.jpg">
+      <p style="width: 30%">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;我觉得吧，
+        当然只是我觉得，
+        每个人都是独立的个体吧，
+        就是说每个人的生活总是会有不一样，
+        我希望能分享你的乐趣，
+        也希望我的乐趣呢，
+        会带给你不一样的惊喜。</p>
+
     </div>
     <div class="text-1" align="center">
-      <div>无忧无虑是三年</div>
-      <div>懵懵懂懂又三年</div>
-      <div>胡思乱想仍三年</div>
-      <div>命里无时终三年</div>
+      <p>无忧无虑是三年<br>
+        懵懵懂懂又三年<br>
+        胡思乱想仍三年<br>
+        命里无时终三年</p>
     </div>
     <div class="text-2" align="center">
-      <div>我呢</div>
-      <div>做不到一蓑烟雨任平生</div>
-      <div>心中又羡慕的紧</div>
-      <div>所以啊</div>
-      <div>人生如逆旅 我亦是行客</div>
+      <p>我呢<br>
+        做不到一蓑烟雨任平生<br>
+        心中又羡慕的紧<br>
+        所以啊<br>
+        人生如逆旅 我亦是行客<br></p>
     </div>
   </div>
 
@@ -46,33 +48,35 @@
   .text{
     background-color:rgba(128,128,128,0.7);;
     color: #87CEFA;
+    width: 60%;
+    height: 40%;
     padding: 50px 40px 50px 40px;
-    width: 800px;
     border-radius:40px;
     font-size: 30px;
     font-weight:400;
+    margin-left: 10%;
   }
   .text-1{
-    background-color:rgba(128,128,128 ,0.7);
+    background-color:rgba(128,128,128,0.7);;
     color: #87CEFA;
-    right: 0;
-    float: right;
-    bottom: 0;
+    width: 15%;
     padding: 50px 40px 50px 40px;
-    width: 500px;
     border-radius:40px;
     font-size: 30px;
     font-weight:400;
+    margin-top: 5%;
+    margin-left: 75%;
   }
   .text-2{
     background-color:rgba(128,128,128 ,0.7);
     color: #87CEFA;
     bottom: 0;
     padding: 50px 40px 50px 40px;
-    width: 500px;
+    width: 25%;
     border-radius:40px;
     font-size: 30px;
     font-weight:400;
+    margin: 5% auto;
   }
 
 
